5654bd901d77cefc6d300c01bd60fcc3eab496d6
lythanhkhiet123 <lythanhkhiet123@gmail.com>
2017-06-24 15:48:59 +1000

    Create Prototype.java
    This is where main class is in.

---
+++ Prototype.java
@@ -0,0 +1,14 @@
+
+package prototype;
+
+
+
+public class Prototype {
+
+    public static void main(String[] args) {
+       
+        
+       MainMenu m1 = new MainMenu();
+       m1.setVisible(true);
+    }
+}
